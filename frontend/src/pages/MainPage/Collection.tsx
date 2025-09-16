561fa625ebfb27e02dc2b1f0703794bec6977bdb
kajol-m <kajolmurmu29gmail.com>
2025-09-16 23:07:21 +0530

    Adding collections

--- frontend/src/pages/MainPage/Collection.tsx
+++ frontend/src/pages/MainPage/Collection.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Button from "../../components/Button/Button";
 
 const Collection: React.FC=()=>{
@@ -5,19 +6,19 @@ const Collection: React.FC=()=>{
         <div className="flex">
         <div className="w-1/4 relative">
             <img src="/Pink-bow-top.webp" alt="Coquette-Collection" className="w-full"/>
-            <Button variant="primary" onClick={() => console.log("Shop Coquette Collection Clicked")} className="absolute bottom-5 left-1/2 transform -translate-x-1/2 -translate-y-1/2">SHOP COQUETTE</Button>
+            <Link  to={`/collections/COL00000001`}><Button variant="primary" onClick={() => console.log("Shop Coquette Collection Clicked")} className="absolute bottom-5 left-1/2 transform -translate-x-1/2 -translate-y-1/2">SHOP PINK</Button></Link>
         </div>
         <div className="w-1/4 relative">
             <img src="/White-hoodie.webp" alt="Coquette-Collection" className="w-full"/>
-            <Button variant="primary" onClick={() => console.log("Shop Coquette Collection Clicked")} className="absolute bottom-5 left-1/2 transform -translate-x-1/2 -translate-y-1/2">SHOP SWEATSHIRT</Button>
+            <Link  to={`/collections/COL00000002`}><Button variant="primary" onClick={() => console.log("Shop Coquette Collection Clicked")} className="absolute bottom-5 left-1/2 transform -translate-x-1/2 -translate-y-1/2">SHOP DENIM</Button></Link>
         </div>
         <div className="w-1/4 relative">
             <img src="/Pink-bow-top.webp" alt="Coquette-Collection" className="w-full"/>
-            <Button variant="primary" onClick={() => console.log("Shop Coquette Collection Clicked")} className="absolute bottom-5 left-1/2 transform -translate-x-1/2 -translate-y-1/2">SHOP COQUETTE</Button>
+            <Link  to={`/collections/COL00000003`}><Button variant="primary" onClick={() => console.log("Shop Coquette Collection Clicked")} className="absolute bottom-5 left-1/2 transform -translate-x-1/2 -translate-y-1/2">SHOP BROWN</Button></Link>
         </div>
         <div className="w-1/4 relative">
             <img src="/White-hoodie.webp" alt="Coquette-Collection" className="w-full"/>
-            <Button variant="primary" onClick={() => console.log("Shop Coquette Collection Clicked")} className="absolute bottom-5 left-1/2 transform -translate-x-1/2 -translate-y-1/2">SHOP SWEATSHIRT</Button>
+            <Link  to={`/collections/COL00000004`}><Button variant="primary" onClick={() => console.log("Shop Coquette Collection Clicked")} className="absolute bottom-5 left-1/2 transform -translate-x-1/2 -translate-y-1/2">SHOP RED</Button></Link>
         </div>
         </div>
     )
